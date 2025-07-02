33e9f7f62dd2fd02424be12d56e979a4a271e38b
saidbataal <saidmt092018@gmail.com>
2025-07-02 04:30:58 +0100

    container work
    select it using queryselector
    craet a 16 div inside it using for loop
    append it to the conatiner

---
+++ script.js
@@ -0,0 +1,18 @@
+const box = document.querySelector(".container");
+
+const numberOfDivs = 16;
+
+for(let i = 0; i < numberOfDivs; i++){
+
+const newDiv  = document.createElement("div");
+
+
+
+box.appendChild(newDiv);
+
+
+
+
+
+
+};
