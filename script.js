af5c9d36094a8604ade08a08f73189d370a7d0a5
saidbataal <saidmt092018@gmail.com>
2025-07-12 04:01:03 +0100

    add hover
    add a class to the divs in the container
    add a foreach and add event listener to the divs to make them change the color when the user hover over the div inside the container

--- script.js
+++ script.js
@@ -16,8 +16,22 @@ newDiv.style.height = size + "px";
 newDiv.style.width = size + "px";
 newDiv.style.boxSizing ="border-box";
 newDiv.style.border = "1px solid black";
-
+newDiv.classList.add("myNewDiv");
 
 
 box.appendChild(newDiv);
 };
+
+
+
+const coloring = document.querySelectorAll(".myNewDiv");
+
+
+
+coloring.forEach(button => {
+    button.addEventListener("mouseenter" , () => {
+        button.style.backgroundColor = "red";
+   });
+});
+
+
